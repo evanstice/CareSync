e9c3764552c33622aa0fbe34a37bb60b25237a6b
tiffanyyvo <tiffanyvo20@yahoo.com>
2024-11-04 11:01:52 -0500

    changed port to 4000

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import cors from "cors"
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.port || 4000
 
 app.use(cors())
 app.use(express.json()); // permits use of .json in 'req' below
@@ -26,7 +26,7 @@ app.use("/api/tasks", taskRoutes)
 
 app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:4000");
 });
 
 
