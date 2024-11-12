9df1a6ccbaa3b78821e46e6e15e4d0109064760e
Ethan King <ethanholtking@gmail.com>
2024-11-12 14:38:50 -0500

    Updated formating of files for login and create account. Default page is login page.

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import Login from '../components/Login/Login'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import NavBar from '../components/Navbar/NavBar';
+
+    return (
+    <>
+        <Login
+        
+        />
+    </>
+    )
