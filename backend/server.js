04ac941edcf4fb555340cbc4c938eae82d3ff97a
tiffanyyvo <tiffanyvo20@yahoo.com>
2024-11-29 13:45:47 -0500

    serverjs

--- backend/server.js
+++ backend/server.js
@@ -30,31 +30,3 @@ app.listen(PORT, () => {
     connectDB();
     console.log("Server started at http://localhost:4000");
 });
-
-
-// import { MongoClient, ServerApiVersion } from 'mongodb';
-// const uri = "mongodb+srv://<db_username>:<db_password>@caresync.tc6eo.mongodb.net/?retryWrites=true&w=majority&appName=CareSync";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
