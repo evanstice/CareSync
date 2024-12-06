24c90678a6ba58d58052ea2b3ab7b467b7d12521
Ethan King <ethanholtking@gmail.com>
2024-12-05 19:03:55 -0500

    fixed route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Route path="/settings" element={<FamilyPage />} />
         <Route path="/delete-account" element={<DeletePage />} />
         <Route path="/update-account" element={<UpdatePage />} />
-        <Route path="/medications" elment={<MedicationsPage />} />
+        <Route path="/medications" element={<MedicationsPage />} />
       </Routes>
     </Router>
   );
